b67270bc01e3a566abb9c0f88776beb687a122f6
Just Me <just806me@gmail.com>
2021-10-09 18:16:36 +0300

    added user secret generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,21 @@ class User < ApplicationRecord
   has_many :solutions, inverse_of: :user
   has_many :results, inverse_of: :user
 
+  before_create :assign_secret
+
   private
 
   def registration_secret_must_be_valid
     unless ActiveSupport::SecurityUtils.secure_compare registration_secret, contest.registration_secret
       errors.add :registration_secret, :invalid
     end
+  end
+
+  def assign_secret
+    self.secret = [
+      (SecureRandom.random_number(26) + 'A'.ord).chr,
+      (SecureRandom.random_number(26) + 'A'.ord).chr,
+      format('%04d', SecureRandom.random_number(10_000)),
+    ].join
   end
 end
