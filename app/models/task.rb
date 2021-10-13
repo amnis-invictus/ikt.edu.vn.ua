af9734ba96f4e6e72c4347dae0cd504b14d55e34
Just Me <just806me@gmail.com>
2021-10-13 22:56:26 +0300

    added accept attribute to file input based on task file names

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,12 @@ class Task < ApplicationRecord
   validates :display_name, presence: true
   validates :file_names, presence: true
   validates :upload_limit, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :contest, inverse_of: :tasks
   has_many :solutions, inverse_of: :task
   has_many :results, inverse_of: :task
+
+  def accepted_ext
+    file_names.map(&File.method(:extname)).join(',') if file_names.present?
+  end
 end
