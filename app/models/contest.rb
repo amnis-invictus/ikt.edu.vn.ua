bb38943993801fc789168b1c40208ab29141533c
Kostiantyn Kostiuk <kostyanf14@live.com>
2021-10-17 23:02:48 +0300

    added contest sites for the contest
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,6 +1,7 @@
 class Contest < ApplicationRecord
   validates :display_name, presence: true
   validates :cities, presence: true
+  validates :contest_sites, presence: true
   has_many_attached :files
   has_many :tasks, inverse_of: :contest
   has_many :users, inverse_of: :contest
