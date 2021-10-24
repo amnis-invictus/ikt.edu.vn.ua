0deacb08ee3776e2f8edd66c1a88ad561c836481
Just Me <just806me@gmail.com>
2021-10-24 18:34:18 +0300

    added fallback to english translation (e.g. for error messages)

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,5 +3,7 @@ Rails.application.configure do
 
   config.i18n.default_locale = :uk
 
+  config.i18n.fallbacks = [:en]
+
   config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 end
