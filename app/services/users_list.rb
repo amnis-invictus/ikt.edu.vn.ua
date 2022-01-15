81e703249868a959060860b696c1acc8a3ee39fb
Just Me <just806me@gmail.com>
2022-01-15 13:59:30 +0200

    added service to generate users list csv

---
+++ app/services/users_list.rb
@@ -0,0 +1,14 @@
+class UsersList
+  HEADERS = %w[secret grade].freeze
+
+  def initialize contest, separator
+    @contest = contest
+    @separator = separator
+  end
+
+  def build_csv
+    CSV.generate headers: HEADERS, write_headers: true, col_sep: @separator do |csv|
+      @contest.users.each { csv << [_1.secret, _1.grade] }
+    end
+  end
+end
