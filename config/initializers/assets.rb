0f5a587b61bf866f08269482e2ee6c9b69ad81fd
Just Me <just806me@gmail.com>
2022-01-21 18:56:45 +0200

    Fix segfault in sassc

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,4 +4,8 @@ Rails.application.configure do
   config.assets.paths << Rails.root.join('node_modules')
 
   config.assets.precompile += %w[home.css font.css]
+
+  config.assets.configure do |env|
+    env.export_concurrent = false
+  end
 end
