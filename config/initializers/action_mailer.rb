15d3a7a6cfe9b94c165aa11d8d982abfa40bcd14
Kostiantyn Kostiuk <kkostiuk@redhat.com>
2022-01-25 10:27:48 +0200

    Fixed rubocop offense
    Signed-off-by: Kostiantyn Kostiuk <kkostiuk@redhat.com>

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   if Rails.env.production?
     config.action_mailer.raise_delivery_errors = true
 
-    config.action_mailer.default_url_options = { host: ENV["APPLICATION_HOST"], protocol: :https }
+    config.action_mailer.default_url_options = { host: ENV['APPLICATION_HOST'], protocol: :https }
 
     config.action_mailer.asset_host = "https://#{ENV['APPLICATION_HOST']}"
   else
