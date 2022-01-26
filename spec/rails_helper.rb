78103c8a812fbd07cf0a67b18284bce89695bfc0
Kostiantyn Kostiuk <konstantin@daynix.com>
2022-01-26 21:39:02 +0000

    rails_spec: Remove redunant assert, just add comment
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,18 +42,14 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before :suite do
-    if config.use_transactional_fixtures?
-      raise <<-MSG
-        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
-        (or set it to false) to prevent uncommitted transactions being used in
-        JavaScript-dependent specs.
+    # Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+    # (or set it to false) to prevent uncommitted transactions being used in
+    # JavaScript-dependent specs.
 
-        During testing, the app-under-test that the browser driver connects to
-        uses a different database connection to the database connection used by
-        the spec. The app's database connection would not be able to access
-        uncommitted transaction data setup over the spec's database connection.
-      MSG
-    end
+    # During testing, the app-under-test that the browser driver connects to
+    # uses a different database connection to the database connection used by
+    # the spec. The app's database connection would not be able to access
+    # uncommitted transaction data setup over the spec's database connection.
 
     DatabaseCleaner.clean_with :truncation
   end
