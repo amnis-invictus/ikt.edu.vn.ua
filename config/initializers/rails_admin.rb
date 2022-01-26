d37d40710a89b2e68c019dc99392147865de65be
Just Me <just806me@gmail.com>
2022-01-26 23:13:08 +0200

    Move cusom rails admin actions & types to /lib, fixes #12

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,5 @@
+Dir[Rails.root.join('lib', 'rails_admin', '**', '*.rb')].each { require _1 }
+
 RailsAdmin::Config::Fields::Types.register :citext, RailsAdmin::Config::Fields::Types::Citext
 RailsAdmin::Config::Fields::Types.register :pg_string_array, RailsAdmin::Config::Fields::Types::PgStringArray
 RailsAdmin::Config::Fields::Types.register :pg_inet_array, RailsAdmin::Config::Fields::Types::PgInetArray
