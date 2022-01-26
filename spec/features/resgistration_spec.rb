66b98b4f77b2972d20565dff32773214d8d41228
Kostiantyn Kostiuk <konstantin@daynix.com>
2022-01-26 22:27:04 +0000

    test: registration: Move before fill_new_user_form to feature scope
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- spec/features/resgistration_spec.rb
+++ spec/features/resgistration_spec.rb
@@ -4,53 +4,54 @@ RSpec.feature 'User registration', type: :feature, ui: true do
   given!(:contest) { create :contest }
 
   before { visit "/contests/#{contest.id}/users/new" }
+  before { fill_new_user_form params }
 
   context 'with everything valid' do
-    before { fill_new_user_form attributes_for :user }
+    given(:params) { attributes_for :user }
     before { click_button 'commit' }
     scenario { expect(page).to have_content 'Ви успішно зареєстровані.' }
   end
 
   context 'with invalid registration secret' do
-    before { fill_new_user_form attributes_for :user, registration_secret: SecureRandom.base36 }
+    given(:params) { attributes_for :user, registration_secret: SecureRandom.base36 }
     before { click_button 'commit' }
     scenario { expect(page).to have_content 'Код доступу помилковий' }
   end
 
   context 'without contest site' do
-    before { fill_new_user_form attributes_for :user, contest_site: nil }
+    given(:params) { attributes_for :user, contest_site: nil }
     before { click_button 'commit' }
     scenario { expect(page).to have_content 'Заклад, у якому ви пишете олімпіаду не може бути порожнім' }
   end
 
   context 'without grade' do
-    before { fill_new_user_form attributes_for :user, grade: nil }
+    given(:params) { attributes_for :user, grade: nil }
     before { click_button 'commit' }
     scenario { expect(page).to have_content 'Клас не може бути порожнім' }
   end
 
   context 'without city' do
-    before { fill_new_user_form attributes_for :user, city: nil }
+    given(:params) { attributes_for :user, city: nil }
     before { click_button 'commit' }
     scenario { expect(page).to have_content 'Місто не може бути порожнім' }
   end
 
   context 'without email' do
-    before { fill_new_user_form attributes_for :user, email: nil }
+    given(:params) { attributes_for :user, email: nil }
     before { click_button 'commit' }
     scenario('should stay on registration page') { expect(page).to have_current_path("/contests/#{contest.id}/users/new") }
     scenario { expect(page).to have_no_content 'Ваш e-mail не може бути порожнім' }
   end
 
   context 'without institution' do
-    before { fill_new_user_form attributes_for :user, institution: nil }
+    given(:params) { attributes_for :user, institution: nil }
     before { click_button 'commit' }
     scenario('should stay on registration page') { expect(page).to have_current_path("/contests/#{contest.id}/users/new") }
     scenario { expect(page).to have_no_content 'Ваш навчальний заклад не може бути порожнім' }
   end
 
   context 'without name' do
-    before { fill_new_user_form attributes_for :user, name: nil }
+    given(:params) { attributes_for :user, name: nil }
     before { click_button 'commit' }
     scenario('should stay on registration page') { expect(page).to have_current_path("/contests/#{contest.id}/users/new") }
     scenario { expect(page).to have_no_content 'Прізвище, Ім\'я, По батькові не може бути пустим' }
