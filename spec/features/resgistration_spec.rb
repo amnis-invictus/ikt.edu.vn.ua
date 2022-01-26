04a369abf305fa3c371fa4e9e12083c280667b19
Kostiantyn Kostiuk <konstantin@daynix.com>
2022-01-26 18:09:41 +0000

    test: registration: added successful test
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

---
+++ spec/features/resgistration_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature 'User registration', type: :feature, ui: true do
+  context 'create new user' do
+    before :each do
+      @contest = create :contest
+
+      visit "/contests/#{@contest.id}/users/new"
+      within 'form' do
+        fill_in 'user_registration_secret', with: @contest.registration_secret
+        fill_in 'user_name', with: 'John Doe'
+        select @contest.cities.last, from: 'user_city'
+        fill_in 'user_institution', with: 'New York Regional Collage'
+        select '10', from: 'user_grade'
+      end
+    end
+
+    scenario 'should be successful' do
+      within 'form' do
+        fill_in 'user_email', with: 'john.doe@example.com'
+        select @contest.contest_sites.first, from: 'user_contest_site'
+      end
+      click_button 'commit'
+      expect(page).to have_content 'Ви успішно зареєстровані.'
+    end
+  end
+end
