beb9de5ca0d17b384be65b623eab57a6c9853bb3
Just Me <just806me@gmail.com>
2022-01-26 21:02:17 +0200

    Use `given` to declare variables in feature specs

--- spec/features/contest_policy_spec.rb
+++ spec/features/contest_policy_spec.rb
@@ -1,64 +1,76 @@
 require 'rails_helper'
 
 RSpec.feature 'Contest policy', type: :feature, ui: true do
-  scenario 'when registration closed' do
-    contest = create :contest, registration_open: false
+  context 'when registration closed' do
+    given(:contest) { create :contest, registration_open: false }
 
-    visit "/contests/#{contest.id}/users/new"
-    expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА'
-    expect(page).to have_content 'Реєстрація заборонена. Повідомте технічного працівника.'
-    expect(page).to have_no_content 'Виберіть місто (район)'
-    expect(page).to have_no_content contest.contest_sites.first
-    expect(page).to have_no_button 'commit'
+    scenario do
+      visit "/contests/#{contest.id}/users/new"
+      expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА'
+      expect(page).to have_content 'Реєстрація заборонена. Повідомте технічного працівника.'
+      expect(page).to have_no_content 'Виберіть місто (район)'
+      expect(page).to have_no_content contest.contest_sites.first
+      expect(page).to have_no_button 'commit'
+    end
   end
 
-  scenario 'when registration open' do
-    contest = create :contest, registration_open: true
+  context 'when registration open' do
+    given(:contest) { create :contest, registration_open: true }
 
-    visit "/contests/#{contest.id}/users/new"
-    expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА'
-    expect(page).to have_no_content 'Реєстрація заборонена. Повідомте технічного працівника.'
-    expect(page).to have_content 'Виберіть місто (район)'
-    expect(page).to have_content contest.contest_sites.first
-    expect(page).to have_button 'commit'
+    scenario do
+      visit "/contests/#{contest.id}/users/new"
+      expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА'
+      expect(page).to have_no_content 'Реєстрація заборонена. Повідомте технічного працівника.'
+      expect(page).to have_content 'Виберіть місто (район)'
+      expect(page).to have_content contest.contest_sites.first
+      expect(page).to have_button 'commit'
+    end
   end
 
   # TODO: Add additional content upload / check
-  scenario 'when task closed' do
-    contest = create :contest, task_open: false
+  context 'when task closed' do
+    given(:contest) { create :contest, task_open: false }
 
-    visit "/contests/#{contest.id}/content"
-    expect(page).to have_content 'Перегляд завдання заборонено. Повідомте технічного працівника.'
-    expect(page).to have_no_content contest.content
-    expect(page).to have_no_content 'Додаткові матеріали'
+    scenario do
+      visit "/contests/#{contest.id}/content"
+      expect(page).to have_content 'Перегляд завдання заборонено. Повідомте технічного працівника.'
+      expect(page).to have_no_content contest.content
+      expect(page).to have_no_content 'Додаткові матеріали'
+    end
   end
 
-  scenario 'when task open' do
-    contest = create :contest, task_open: true
+  context 'when task open' do
+    given(:contest) { create :contest, task_open: true }
 
-    visit "/contests/#{contest.id}/content"
-    expect(page).to have_no_content 'Перегляд завдання заборонено. Повідомте технічного працівника.'
-    expect(page).to have_content contest.content
-    expect(page).to have_content 'Додаткові матеріали'
+    scenario do
+      visit "/contests/#{contest.id}/content"
+      expect(page).to have_no_content 'Перегляд завдання заборонено. Повідомте технічного працівника.'
+      expect(page).to have_content contest.content
+      expect(page).to have_content 'Додаткові матеріали'
+    end
   end
 
-  scenario 'when upload closed' do
-    contest = create :contest, upload_open: false
+  context 'when upload closed' do
+    given(:contest) { create :contest, upload_open: false }
 
-    visit "/contests/#{contest.id}/upload/new"
-    expect(page).to have_content 'ВІДПРАВКА РОЗВ\'ЯЗКІВ'
-    expect(page).to have_content 'Відправка розв\'язків заборонена. Повідомте технічного працівника.'
-    expect(page).to have_no_field 'upload_secret'
-    expect(page).to have_no_button 'commit'
+    scenario do
+      visit "/contests/#{contest.id}/upload/new"
+      expect(page).to have_content 'ВІДПРАВКА РОЗВ\'ЯЗКІВ'
+      expect(page).to have_content 'Відправка розв\'язків заборонена. Повідомте технічного працівника.'
+      expect(page).to have_no_field 'upload_secret'
+      expect(page).to have_no_button 'commit'
+    end
   end
 
-  scenario 'when upload open' do
-    contest = create :contest, upload_open: true
+  context 'when upload open' do
+    given(:contest) { create :contest, upload_open: true }
 
-    visit "/contests/#{contest.id}/upload/new"
-    expect(page).to have_content 'ВІДПРАВКА РОЗВ\'ЯЗКІВ'
-    expect(page).to have_no_content 'Відправка розв\'язків заборонена. Повідомте технічного працівника.'
-    expect(page).to have_field 'upload_secret'
-    expect(page).to have_button 'commit'
+    scenario do
+      visit "/contests/#{contest.id}/upload/new"
+      expect(page).to have_content 'ВІДПРАВКА РОЗВ\'ЯЗКІВ'
+      expect(page).to have_no_content 'Відправка розв\'язків заборонена. Повідомте технічного працівника.'
+      expect(page).to have_field 'upload_secret'
+      expect(page).to have_button 'commit'
+    end
   end
 end
--- spec/features/resgistration_spec.rb
+++ spec/features/resgistration_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.feature 'User registration', type: :feature, ui: true do
   context 'create new user' do
-    before :each do
-      @contest = create :contest
+    given(:contest) { create :contest }
 
-      visit "/contests/#{@contest.id}/users/new"
+    before :each do
+      visit "/contests/#{contest.id}/users/new"
       within 'form' do
-        fill_in 'user_registration_secret', with: @contest.registration_secret
+        fill_in 'user_registration_secret', with: contest.registration_secret
         fill_in 'user_name', with: 'John Doe'
-        select @contest.cities.last, from: 'user_city'
+        select contest.cities.last, from: 'user_city'
         fill_in 'user_institution', with: 'New York Regional Collage'
         select '10', from: 'user_grade'
       end
@@ -18,7 +18,7 @@ RSpec.feature 'User registration', type: :feature, ui: true do
     scenario 'should be successful' do
       within 'form' do
         fill_in 'user_email', with: 'john.doe@example.com'
-        select @contest.contest_sites.first, from: 'user_contest_site'
+        select contest.contest_sites.first, from: 'user_contest_site'
       end
       click_button 'commit'
       expect(page).to have_content 'Ви успішно зареєстровані.'
@@ -26,9 +26,9 @@ RSpec.feature 'User registration', type: :feature, ui: true do
 
     scenario 'with wrong registration_secret should fail' do
       within 'form' do
-        fill_in 'user_registration_secret', with: @contest.registration_secret.reverse
+        fill_in 'user_registration_secret', with: contest.registration_secret.reverse
         fill_in 'user_email', with: 'john.doe@example.com'
-        select @contest.contest_sites.first, from: 'user_contest_site'
+        select contest.contest_sites.first, from: 'user_contest_site'
       end
       click_button 'commit'
       expect(page).to have_content 'Код доступу помилковий'
@@ -44,7 +44,7 @@ RSpec.feature 'User registration', type: :feature, ui: true do
 
     scenario 'without user_email should fail (no post)' do
       within 'form' do
-        select @contest.contest_sites.first, from: 'user_contest_site'
+        select contest.contest_sites.first, from: 'user_contest_site'
       end
       click_button 'commit'
       expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА'
