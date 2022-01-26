7706cabffe927c9678fafd66447d1ca60f720926
Kostiantyn Kostiuk <konstantin@daynix.com>
2022-01-26 18:12:29 +0000

    test: contest policy: added when registration open test
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- spec/features/contest_policy_spec.rb
+++ spec/features/contest_policy_spec.rb
@@ -11,4 +11,15 @@ RSpec.feature 'Contest policy', type: :feature, ui: true do
     expect(page).to have_no_content contest.contest_sites.first
     expect(page).to have_no_button 'commit'
   end
+
+  scenario 'when registration open' do
+    contest = create :contest, registration_open: true
+
+    visit "/contests/#{contest.id}/users/new"
+    expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА'
+    expect(page).to have_no_content 'Реєстрація заборонена. Повідомте технічного працівника.'
+    expect(page).to have_content 'Виберіть місто (район)'
+    expect(page).to have_content contest.contest_sites.first
+    expect(page).to have_button 'commit'
+  end
 end
