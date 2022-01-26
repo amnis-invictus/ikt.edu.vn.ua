f139aeab051d6d9c7ca57b018459e9ce45a8ee85
Just Me <just806me@gmail.com>
2022-01-26 21:34:18 +0200

    test: Group contest policy specs, split scenarios

--- spec/features/contest_policy_spec.rb
+++ spec/features/contest_policy_spec.rb
@@ -1,76 +1,70 @@
 require 'rails_helper'
 
-RSpec.feature 'Contest policy', type: :feature, ui: true do
-  context 'when registration closed' do
-    given(:contest) { create :contest, registration_open: false }
+RSpec.feature 'ContestPolicy', type: :feature, ui: true do
+  feature 'registration' do
+    before { visit "/contests/#{contest.id}/users/new" }
 
-    scenario do
-      visit "/contests/#{contest.id}/users/new"
-      expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА'
-      expect(page).to have_content 'Реєстрація заборонена. Повідомте технічного працівника.'
-      expect(page).to have_no_content 'Виберіть місто (район)'
-      expect(page).to have_no_content contest.contest_sites.first
-      expect(page).to have_no_button 'commit'
+    context 'when closed' do
+      given(:contest) { create :contest, registration_open: false }
+
+      scenario { expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА' }
+      scenario { expect(page).to have_content 'Реєстрація заборонена. Повідомте технічного працівника.' }
+      scenario { expect(page).to have_no_content 'Виберіть місто (район)' }
+      scenario { expect(page).to have_no_content contest.contest_sites.first }
+      scenario { expect(page).to have_no_button 'commit' }
     end
-  end
 
-  context 'when registration open' do
-    given(:contest) { create :contest, registration_open: true }
+    context 'when open' do
+      given(:contest) { create :contest, registration_open: true }
 
-    scenario do
-      visit "/contests/#{contest.id}/users/new"
-      expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА'
-      expect(page).to have_no_content 'Реєстрація заборонена. Повідомте технічного працівника.'
-      expect(page).to have_content 'Виберіть місто (район)'
-      expect(page).to have_content contest.contest_sites.first
-      expect(page).to have_button 'commit'
+      scenario { expect(page).to have_content 'РЕЄСТРАЦІЯ УЧАСНИКА' }
+      scenario { expect(page).to have_no_content 'Реєстрація заборонена. Повідомте технічного працівника.' }
+      scenario { expect(page).to have_content 'Виберіть місто (район)' }
+      scenario { expect(page).to have_content contest.contest_sites.first }
+      scenario { expect(page).to have_button 'commit' }
     end
   end
 
   # TODO: Add additional content upload / check
-  context 'when task closed' do
-    given(:contest) { create :contest, task_open: false }
+  feature 'viewing content' do
+    before { visit "/contests/#{contest.id}/content" }
 
-    scenario do
-      visit "/contests/#{contest.id}/content"
-      expect(page).to have_content 'Перегляд завдання заборонено. Повідомте технічного працівника.'
-      expect(page).to have_no_content contest.content
-      expect(page).to have_no_content 'Додаткові матеріали'
+    context 'when task closed' do
+      given(:contest) { create :contest, task_open: false }
+
+      scenario { expect(page).to have_content 'Перегляд завдання заборонено. Повідомте технічного працівника.' }
+      scenario { expect(page).to have_no_content contest.content }
+      scenario { expect(page).to have_no_content 'Додаткові матеріали' }
+    end
+
+    context 'when task open' do
+      given(:contest) { create :contest, task_open: true }
+
+      scenario { expect(page).to have_no_content 'Перегляд завдання заборонено. Повідомте технічного працівника.' }
+      scenario { expect(page).to have_content contest.content }
+      scenario { expect(page).to have_content 'Додаткові матеріали' }
     end
   end
 
-  context 'when task open' do
-    given(:contest) { create :contest, task_open: true }
+  feature 'upload' do
+    before { visit "/contests/#{contest.id}/upload/new" }
 
-    scenario do
-      visit "/contests/#{contest.id}/content"
-      expect(page).to have_no_content 'Перегляд завдання заборонено. Повідомте технічного працівника.'
-      expect(page).to have_content contest.content
-      expect(page).to have_content 'Додаткові матеріали'
+    context 'when closed' do
+      given(:contest) { create :contest, upload_open: false }
+
+      scenario { expect(page).to have_content 'ВІДПРАВКА РОЗВ\'ЯЗКІВ' }
+      scenario { expect(page).to have_content 'Відправка розв\'язків заборонена. Повідомте технічного працівника.' }
+      scenario { expect(page).to have_no_field 'upload_secret' }
+      scenario { expect(page).to have_no_button 'commit' }
     end
-  end
 
-  context 'when upload closed' do
-    given(:contest) { create :contest, upload_open: false }
+    context 'when open' do
+      given(:contest) { create :contest, upload_open: true }
 
-    scenario do
-      visit "/contests/#{contest.id}/upload/new"
-      expect(page).to have_content 'ВІДПРАВКА РОЗВ\'ЯЗКІВ'
-      expect(page).to have_content 'Відправка розв\'язків заборонена. Повідомте технічного працівника.'
-      expect(page).to have_no_field 'upload_secret'
-      expect(page).to have_no_button 'commit'
-    end
-  end
-
-  context 'when upload open' do
-    given(:contest) { create :contest, upload_open: true }
-
-    scenario do
-      visit "/contests/#{contest.id}/upload/new"
-      expect(page).to have_content 'ВІДПРАВКА РОЗВ\'ЯЗКІВ'
-      expect(page).to have_no_content 'Відправка розв\'язків заборонена. Повідомте технічного працівника.'
-      expect(page).to have_field 'upload_secret'
-      expect(page).to have_button 'commit'
+      scenario { expect(page).to have_content 'ВІДПРАВКА РОЗВ\'ЯЗКІВ' }
+      scenario { expect(page).to have_no_content 'Відправка розв\'язків заборонена. Повідомте технічного працівника.' }
+      scenario { expect(page).to have_field 'upload_secret' }
+      scenario { expect(page).to have_button 'commit' }
     end
   end
 end
