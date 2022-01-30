dd332d96e99bd95a294e7d811ab211d957192ecb
Just Me <just806me@gmail.com>
2022-01-30 02:11:42 +0200

    Add api authentication

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,12 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :task
+
+    def connect
+      password, task_id = request.params.values_at :password, :task_id
+      @task = Task.find_by id: task_id if password.present? && task_id.present?
+      password_valid = ActiveSupport::SecurityUtils.secure_compare password, @task.contest.judge_password if @task
+      reject_unauthorized_connection unless password_valid
+    end
   end
 end
