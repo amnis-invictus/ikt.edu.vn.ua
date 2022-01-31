e4de48635a7bc06617631a425b1fdd1683efb5ec
Kostiantyn Kostiuk <kostyanf14@live.com>
2022-01-31 06:35:55 +0000

    Fix attachments spelling
    Break backward compatibility
    
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -5,10 +5,10 @@ class Contest < ApplicationRecord
   has_one_attached :last_archive
   has_one_attached :author_solution
   has_one_attached :judge_solution
-  has_one_attached :criterias
+  has_one_attached :criteria
   has_one_attached :preliminary_results
   has_one_attached :final_results
-  has_many_attached :additinal_content
+  has_many_attached :additional_content
 
   has_many :tasks, inverse_of: :contest, dependent: :destroy
   has_many :users, inverse_of: :contest, dependent: :destroy
