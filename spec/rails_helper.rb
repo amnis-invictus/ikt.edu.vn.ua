4895e57ded039e42a52a14b998c4c7c1ace7ac5a
Kostiantyn Kostiuk <kostyanf14@live.com>
2022-02-02 16:59:10 +0000

    test: Add registration form helper
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,7 @@ require 'database_cleaner/active_record'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { require _1 }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -107,6 +107,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  config.include FormHelpers
 end
 
 browser = ENV['UI_TEST_ENGINE']&.downcase == 'chrome' ? :chrome : :firefox
