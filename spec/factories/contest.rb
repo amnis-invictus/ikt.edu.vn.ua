c84ab2c1f9259444f73683e9fd9e8ac5cd55aaab
Kostiantyn Kostiuk <kostyanf14@live.com>
2022-02-02 16:47:32 +0000

    test: added default contest factory
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

---
+++ spec/factories/contest.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :contest do
+    display_name { '1st USA olympiad' }
+    cities { ['New York', 'Los Angeles', 'Chicago'] }
+    contest_sites { ['New York School', 'Los Angeles College', 'Chicago University'] }
+    registration_secret { 'ABC123' }
+    registration_open { true }
+    task_open { true }
+    upload_open { true }
+    judge_password { 'password' }
+    content { '<h2>What is Lorem Ipsum?</h2><p>Lorem <b>Ipsum</b> is simply dummy text of the industry.</p>' }
+  end
+end
