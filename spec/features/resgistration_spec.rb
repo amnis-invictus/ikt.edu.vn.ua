8215a632c30298d47f6b1902a2d1038fd2a94cce
Kostiantyn Kostiuk <kostyanf14@live.com>
2022-02-09 18:04:40 +0000

    tests: Add user registration for contest without contest_site
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- spec/features/resgistration_spec.rb
+++ spec/features/resgistration_spec.rb
@@ -69,4 +69,24 @@ RSpec.feature 'User registration', type: :feature, ui: true do
       scenario { expect(page).to have_no_content 'Прізвище, Ім\'я, По батькові не може бути пустим' }
     end
   end
+
+  context 'for contest without contest_site' do
+    given!(:contest) { create :contest, contest_sites: [] }
+
+    before { fill_inputs 'user', params.slice(:name, :institution, :email, :contest_site, :registration_secret) }
+    before { fill_selects 'user', params.slice(:city, :grade) }
+
+    context 'with everything valid' do
+      given(:params) { attributes_for :user }
+      before { click_button 'commit' }
+      scenario { expect(page).to have_content 'Ви успішно зареєстровані.' }
+    end
+
+    context 'without contest_site' do
+      given(:params) { attributes_for :user, contest_site: nil }
+      before { click_button 'commit' }
+      scenario('should stay on registration page') { expect(page).to have_current_path(registration_path) }
+      scenario { expect(page).to have_no_content 'Заклад, у якому ви пишете олімпіаду не може бути порожнім' }
+    end
+  end
 end
