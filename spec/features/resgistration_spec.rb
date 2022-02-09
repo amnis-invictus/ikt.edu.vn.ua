16a3060ffbae1635082515aa27f21d57f05bfcca
Kostiantyn Kostiuk <kostyanf14@live.com>
2022-02-09 17:56:41 +0000

    tests: Move user registration to additional context
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- spec/features/resgistration_spec.rb
+++ spec/features/resgistration_spec.rb
@@ -1,69 +1,72 @@
 require 'rails_helper'
 
 RSpec.feature 'User registration', type: :feature, ui: true do
-  given!(:contest) { create :contest }
   given(:registration_path) { "/contests/#{contest.id}/users/new" }
-
   before { visit registration_path }
-  before { fill_inputs 'user', params.slice(:name, :institution, :email, :registration_secret) }
-  before { fill_selects 'user', params.slice(:city, :contest_site, :grade) }
 
-  context 'with everything valid' do
-    given(:params) { attributes_for :user }
-    before { click_button 'commit' }
-    scenario { expect(page).to have_content 'Ви успішно зареєстровані.' }
-  end
+  context 'for contest with contest_site and city' do
+    given!(:contest) { create :contest }
 
-  context 'with duplicated name' do
-    before { create :user, contest: }
-    given(:params) { attributes_for :user }
-    before { click_button 'commit' }
+    before { fill_inputs 'user', params.slice(:name, :institution, :email, :registration_secret) }
+    before { fill_selects 'user', params.slice(:city, :contest_site, :grade) }
 
-    scenario { expect(page).to have_content "Учасник на ім'я #{params[:name]} вже зареєстрований" }
-  end
+    context 'with everything valid' do
+      given(:params) { attributes_for :user }
+      before { click_button 'commit' }
+      scenario { expect(page).to have_content 'Ви успішно зареєстровані.' }
+    end
 
-  context 'with invalid registration secret' do
-    given(:params) { attributes_for :user, registration_secret: SecureRandom.base36 }
-    before { click_button 'commit' }
-    scenario { expect(page).to have_content 'Код доступу помилковий' }
-  end
+    context 'with duplicated name' do
+      before { create :user, contest: }
+      given(:params) { attributes_for :user }
+      before { click_button 'commit' }
 
-  context 'without contest site' do
-    given(:params) { attributes_for :user, contest_site: nil }
-    before { click_button 'commit' }
-    scenario { expect(page).to have_content 'Заклад, у якому ви пишете олімпіаду не може бути порожнім' }
-  end
+      scenario { expect(page).to have_content "Учасник на ім'я #{params[:name]} вже зареєстрований" }
+    end
 
-  context 'without grade' do
-    given(:params) { attributes_for :user, grade: nil }
-    before { click_button 'commit' }
-    scenario { expect(page).to have_content 'Клас не може бути порожнім' }
-  end
+    context 'with invalid registration secret' do
+      given(:params) { attributes_for :user, registration_secret: SecureRandom.base36 }
+      before { click_button 'commit' }
+      scenario { expect(page).to have_content 'Код доступу помилковий' }
+    end
 
-  context 'without city' do
-    given(:params) { attributes_for :user, city: nil }
-    before { click_button 'commit' }
-    scenario { expect(page).to have_content 'Місто не може бути порожнім' }
-  end
+    context 'without contest site' do
+      given(:params) { attributes_for :user, contest_site: nil }
+      before { click_button 'commit' }
+      scenario { expect(page).to have_content 'Заклад, у якому ви пишете олімпіаду не може бути порожнім' }
+    end
 
-  context 'without email' do
-    given(:params) { attributes_for :user, email: nil }
-    before { click_button 'commit' }
-    scenario('should stay on registration page') { expect(page).to have_current_path(registration_path) }
-    scenario { expect(page).to have_no_content 'Ваш e-mail не може бути порожнім' }
-  end
+    context 'without grade' do
+      given(:params) { attributes_for :user, grade: nil }
+      before { click_button 'commit' }
+      scenario { expect(page).to have_content 'Клас не може бути порожнім' }
+    end
 
-  context 'without institution' do
-    given(:params) { attributes_for :user, institution: nil }
-    before { click_button 'commit' }
-    scenario('should stay on registration page') { expect(page).to have_current_path(registration_path) }
-    scenario { expect(page).to have_no_content 'Ваш навчальний заклад не може бути порожнім' }
-  end
+    context 'without city' do
+      given(:params) { attributes_for :user, city: nil }
+      before { click_button 'commit' }
+      scenario { expect(page).to have_content 'Місто не може бути порожнім' }
+    end
 
-  context 'without name' do
-    given(:params) { attributes_for :user, name: nil }
-    before { click_button 'commit' }
-    scenario('should stay on registration page') { expect(page).to have_current_path(registration_path) }
-    scenario { expect(page).to have_no_content 'Прізвище, Ім\'я, По батькові не може бути пустим' }
+    context 'without email' do
+      given(:params) { attributes_for :user, email: nil }
+      before { click_button 'commit' }
+      scenario('should stay on registration page') { expect(page).to have_current_path(registration_path) }
+      scenario { expect(page).to have_no_content 'Ваш e-mail не може бути порожнім' }
+    end
+
+    context 'without institution' do
+      given(:params) { attributes_for :user, institution: nil }
+      before { click_button 'commit' }
+      scenario('should stay on registration page') { expect(page).to have_current_path(registration_path) }
+      scenario { expect(page).to have_no_content 'Ваш навчальний заклад не може бути порожнім' }
+    end
+
+    context 'without name' do
+      given(:params) { attributes_for :user, name: nil }
+      before { click_button 'commit' }
+      scenario('should stay on registration page') { expect(page).to have_current_path(registration_path) }
+      scenario { expect(page).to have_no_content 'Прізвище, Ім\'я, По батькові не може бути пустим' }
+    end
   end
 end
