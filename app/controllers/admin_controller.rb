b0624bc56bec83dfda858f578d2ce0d02135614e
Just Me <just806me@gmail.com>
2022-06-22 22:41:49 +0300

    Disable Rails/ApplicationController for AdminController only

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,5 @@
-class AdminController < ActionController::Base
+# This controller is used only as a parent controller for Rails Admin. We don't want any logic from ApplicationController
+# and the default option is `::ActionController::Base`, so it should be OK.
+class AdminController < ActionController::Base # rubocop:disable Rails/ApplicationController
   before_action { I18n.locale = :en }
 end
