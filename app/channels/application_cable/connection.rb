f07f2cc4c0f5975bd3c219f716300db2b97952bb
Just Me <just806me@gmail.com>
2022-12-02 15:07:23 +0200

    Refactor ApplicationCable::Connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,22 +3,34 @@ module ApplicationCable
     identified_by :task_id, :client_id
 
     def connect
-      token, password, @task_id, @client_id = request.params.values_at :token, :password, :task_id, :client_id
-
-      auth_valid = false
-      if token.present?
-        auth_valid = Rails.application.message_verifier(:scoring).verified(token) == @task_id
-      elsif password.present?
-        task = Task.find_by id: @task_id if @task_id.present?
-        auth_valid = ActiveSupport::SecurityUtils.secure_compare password, task.contest.judge_password if task
-      end
-
-      reject_unauthorized_connection unless auth_valid
+      @task_id, @client_id = request.params.values_at :task_id, :client_id
+      reject_unauthorized_connection unless task_valid && (token_valid || password_valid)
     end
 
     def disconnect
       RedisLockManager.release_all client_id
       ActionCable.server.broadcast task_id, { type: 'locks/load', payload: RedisLockManager.all }
+    end
+
+    private
+
+    def task_valid
+      @task = Task.find_by id: task_id if task_id.present?
+      @task.present?
+    end
+
+    def token_valid
+      token = request.params[:token]
+      return false if token.blank?
+
+      Rails.application.message_verifier(:scoring).verified(token) == task_id
+    end
+
+    def password_valid
+      password = request.params[:password]
+      return false if password.blank?
+
+      ActiveSupport::SecurityUtils.secure_compare password, @task.contest.judge_password
     end
   end
 end
