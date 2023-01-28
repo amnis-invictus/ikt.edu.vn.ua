6a22beef95a078eb247862aa4fcd06669bedc8d2
Konstantin Kostiuk <kkostiuk@redhat.com>
2023-01-28 23:11:30 +0200

    RedisManager: rename release_all to be consistent
    Signed-off-by: Konstantin Kostiuk <kkostiuk@redhat.com>

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,7 +8,7 @@ module ApplicationCable
     end
 
     def disconnect
-      RedisLockManager.release_all client_id
+      RedisLockManager.release_all_by_client client_id
       ActionCable.server.broadcast task_id, { type: 'locks/load', payload: RedisLockManager.all }
     end
 
