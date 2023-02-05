b2588842028569b46caf322cb6909950dea269af
Kostiantyn Kostiuk <kostyanf14@live.com>
2023-02-05 14:13:49 +0200

    Use judge secret in penalty screens
    In general penalty screens are used by judges and judges
    can't check information with an upload secret.
    
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- lib/rails_admin/config/actions/device_ids_mismatch.rb
+++ lib/rails_admin/config/actions/device_ids_mismatch.rb
@@ -15,8 +15,11 @@ module RailsAdmin
             @collection = @object.solutions
               .where('solutions.device_id != users.device_id')
               .group('users.id')
-              .pluck('users.device_id', 'users.secret', 'array_agg(solutions.id)')
-              .map { |did, secret, solution_ids| [did, secret, Solution.includes(:task).where(id: solution_ids)] }
+              .pluck('users.device_id', 'users.judge_secret', 'array_agg(solutions.id)')
+              .map do |did, judge_secret, solution_ids|
+              [did, judge_secret,
+                Solution.includes(:task).where(id: solution_ids)]
+            end
           end
         end
       end
--- lib/rails_admin/config/actions/ips_mismatch.rb
+++ lib/rails_admin/config/actions/ips_mismatch.rb
@@ -15,8 +15,11 @@ module RailsAdmin
             @collection = @object.solutions
               .where('solutions.ips != users.registration_ips')
               .group('users.id')
-              .pluck('users.registration_ips', 'users.secret', 'array_agg(solutions.id)')
-              .map { |ips, secret, solution_ids| [ips, secret, Solution.includes(:task).where(id: solution_ids)] }
+              .pluck('users.registration_ips', 'users.judge_secret', 'array_agg(solutions.id)')
+              .map do |ips, judge_secret, solution_ids|
+              [ips, judge_secret,
+                Solution.includes(:task).where(id: solution_ids)]
+            end
           end
         end
       end
