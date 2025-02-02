a040859126693901008e3c4f5426145f2d1d17ef
Just Me <just806me@gmail.com>
2025-02-03 00:18:22 +0200

    Update sidebar navigation, add orgcom routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,12 @@ Rails.application.routes.draw do
       get :users_csv
       get :judge_xlsx
     end
+
+    resource :orgcom, only: [], module: :orgcom do
+      resource :session, path: '', as: '', only: %i[show destroy]
+      resource :contest, only: %i[edit update]
+      resources :users, only: %i[index new create edit update]
+    end
   end
 
   resource :timer, only: :show
