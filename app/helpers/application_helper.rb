d2a09a5eda73648b05190018bca44f4d35699225
Just Me <just806me@gmail.com>
2025-02-04 18:32:54 +0200

    Load orgcom assets only in orgcom controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     controller_name == 'contests' && action_name == 'index'
   end
 
+  def orgcom_module?
+    controller.class.name.start_with? 'Orgcom::'
+  end
+
   def scoring_url task
     token = Rails.application.message_verifier(:scoring).generate(task.id.to_s, expires_in: 1.hour)
 
